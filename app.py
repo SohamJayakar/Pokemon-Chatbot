8a1970b8b7c79ec918a110f93a7c915cf280adcc
Soham Jayakar <sohamjayakar@gmail.com>
2025-06-09 19:23:19 +0530

    Update .gitignore, enhance app configuration, and modify index.html. Added more ignored files, implemented environment variable support for Flask app, and added a favicon link in the HTML template. Removed unused image asset and updated requirements with gunicorn.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify, url_for
 from pokemon_chatbot import PokemonChatbot
+import os
 
 app = Flask(__name__, static_url_path='/static')
 chatbot = PokemonChatbot()
@@ -18,4 +19,8 @@ def chat():
     return jsonify({'response': response})
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    # Use environment variable for port with a default value
+    port = int(os.environ.get('PORT', 5000))
+    # In production, debug should be False
+    debug = os.environ.get('FLASK_ENV') == 'development'
+    app.run(host='0.0.0.0', port=port, debug=debug) 
